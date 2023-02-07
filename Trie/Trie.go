5e4eb3a9d1ecc906fcb80ec5d8a0cf9c7c9ff954
libowei <libowei0702@163.com>
2023-02-07 22:51:01 +0800

    Create Trie.go

---
+++ Trie/Trie.go
@@ -0,0 +1,23 @@
+type Trie struct {
+
+}
+
+
+func Constructor() Trie {
+
+}
+
+
+func (this *Trie) Insert(word string)  {
+
+}
+
+
+func (this *Trie) Search(word string) bool {
+
+}
+
+
+func (this *Trie) StartsWith(prefix string) bool {
+
+}
